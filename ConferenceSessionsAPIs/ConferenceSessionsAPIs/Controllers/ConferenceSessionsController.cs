f39788631abda78e1f275d10b0428dcf57e552a0
darkeung <hawygfau@gmail.com>
2018-09-03 04:57:43 +1000

    wk6

---
+++ ConferenceSessionsAPIs/ConferenceSessionsAPIs/Controllers/ConferenceSessionsController.cs
@@ -0,0 +1,30 @@
+﻿using ConferenceSessionsAPIs.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+
+namespace ConferenceSessionsAPIs.Controllers
+{
+    public class ConferenceSessionsController : ApiController
+    {
+        List<Session> sessions = new List<Session>(
+            new Session[]
+        {
+            new Session{SessionTitle = "Microsoft", SessionDescription="Azure!"},
+            new Session{SessionTitle = "Google", SessionDescription="Android!"},
+            new Session{SessionTitle = "Facebook", SessionDescription="What's APP!"},
+            new Session{SessionTitle = "IBM", SessionDescription="Watson"}
+        }
+        );
+        public List<Session> GetAllClass()
+        {
+            return sessions;
+        }
+        public Session GetSession(string id)
+        {
+            var session = sessions.FirstOrDefault((p) => p.SessionTitle == id);
+            return session;
+        }
+    }
+}
